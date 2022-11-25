e6e578d3a338e8e1474cd2bc4fa0106df9a710e7
Muthukrishnan <muthukrishnan@luminescent.digital>
2022-11-25 13:16:54 +0530

    Add router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,13 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import ModelParent from "./components/ModelParent";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
+
 import { useState } from "react";
 
 function App() {
@@ -16,22 +23,37 @@ function App() {
   return (
     <div className="App">
       <Header cartQty={qty} className="App-header" />
-      <Layout>
-        <div className="row">
-          <div className="col-sm-8">
-            <h2>Main component</h2>
-            <div className="mt-5"></div>
-            <button onClick={addCart} type="button" className="btn btn-success">
-              Add 1+ to cart
-            </button>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="*" element={<NoPage />} />
+            
+            {/*  <Route path="/" element={<Layout />}>
+        </Route> */}
+          </Routes>
+          <div className="row">
+            <div className="col-sm-8">
+              <h2>Main component</h2>
+              <div className="mt-5"></div>
+              <button
+                onClick={addCart}
+                type="button"
+                className="btn btn-success"
+              >
+                Add 1+ to cart
+              </button>
+            </div>
+            <div className="col-sm-4">
+              <ModelParent />
+              <ModelParent />
+              <ModelParent />
+            </div>
           </div>
-          <div className="col-sm-4">
-            <ModelParent />
-            <ModelParent />
-            <ModelParent />
-          </div>
-        </div>
-      </Layout>
+        </Layout>
+      </BrowserRouter>
       <Footer className="App-footer" />
     </div>
   );
